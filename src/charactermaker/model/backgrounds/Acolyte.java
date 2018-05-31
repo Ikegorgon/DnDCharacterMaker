9793d593c8bba1fa6e21d32e01e1007d809c1af7
isaac <isaac@IsaacsComputer>
2018-05-31 11:17:49 -0600

    ++

--- src/charactermaker/model/backgrounds/Acolyte.java
+++ src/charactermaker/model/backgrounds/Acolyte.java
@@ -4,14 +4,14 @@ import charactermaker.model.items.*;
 import charactermaker.model.features.feats.backgroundfeat.*;
 import java.util.*;
 
-public class AAA extends Backgrounds {
+public class Acolyte extends Backgrounds {
 	private List<BackgroundFeats> feats;
 	private List<String> proficiency;
 	private List<Items> equipment;
-	public AAA() {
-//		super("");
+	public Acolyte() {
+		super("Acolyte");
 //		this.setDescription("");
-//		this.setBook("");
+//		this.setBook("PHB");
 		feats = new ArrayList<BackgroundFeats>();
 //		feats.add();
 		proficiency = new ArrayList<String>();
